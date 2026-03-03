cfa497960984cff165868ca6d9dd913daf79ce57
Maxim <drensymax@gmail.com>
2026-03-03 20:56:37 +0300

    1.4
    - color, fontSize, letterSpacing, textAlign (только по горизонтали), lineHight, maxLines, overflow.
    - onTextLayout - коллбэк после отрисовки текста (сделали через запоминание количества строк и вывода тоста когда это число изменится через 5 секунд)

--- app/src/main/java/com/example/practicumcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicumcompose/MainActivity.kt
@@ -14,10 +14,23 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,22 +46,65 @@ class MainActivity : ComponentActivity() {
 
         val context = LocalContext.current
 
-        Text(
-            text = "Hello",
-            modifier = Modifier
-                .padding(16.dp)
-                .systemBarsPadding() // 👈 ВАЖНО! Добавляет отступы для статус бара и навигации оставляем  enableEdgeToEdge()
+        var text by remember { mutableStateOf("Hello") }
+        val lastLineCount = remember { mutableIntStateOf(0) }
 
+        LaunchedEffect(Unit) {
+            delay(5000)
+            text = "Very long text with many different lines and styles"
+        }
+
+        Text(
+            text = text,
+            modifier = Modifier
+                .fillMaxSize() // растянуть на всю ширину
+                .padding(16.dp)
+                .systemBarsPadding() // 👈 ВАЖНО! Добавляет отступы для статус бара и навигации если ставим enableEdgeToEdge()
                 .background(
                     color = Color(0xFFE1F5FE),
                     shape = RoundedCornerShape(12.dp)
                 )
                 .padding(12.dp)
+//                .wrapContentSize(align = Alignment.Center)
 
-                .fillMaxSize() // растянуть на всю ширину
                 .clickable {
                     context.showToast("Клик сделан")
+                },
+
+            color = Color(0xFF01579B), // цвет текста
+            fontSize = 27.sp,
+
+//          //  (стиль: курсив или нормальный)
+//          fontStyle = FontStyle.Italic,    // курсив
+//          // жирность
+//          fontWeight = FontWeight.Bold,    // жирный (можно Medium, Light, и т.д.)
+//          // семейство шрифтов
+//          fontFamily = FontFamily.SansSerif, // или Serif, Monospace
+//          // подчеркивание, зачеркивание
+//          textDecoration = TextDecoration.Underline, // подчеркнутый
+
+            letterSpacing = 16.sp,
+            textAlign = TextAlign.Center, // выравниваем ТОЛЬКО по горизонтали
+            lineHeight = 32.sp, // расстояние между строками
+            maxLines = 3,
+            overflow = TextOverflow.Ellipsis, // что делать если текст не влезает
+
+            // коллбэк после отрисовки текста
+            onTextLayout = { textLayoutResult ->
+                val lineCount: Int = textLayoutResult.lineCount
+                if (lastLineCount.intValue != lineCount) {
+                    lastLineCount.intValue = lineCount
+                    context.showToast("Lines number = ${lineCount}")
                 }
+            },
+
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Red,
+                    offset = Offset(4f, 7f),
+                    blurRadius = 8f
+                )
+            )
         )
     }
 
