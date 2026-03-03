5e9e6e0d4149f71d71123bd978897d0bd1ffc972
Maxim <drensymax@gmail.com>
2026-03-03 19:56:50 +0300

    1.2
    - .systemBarsPadding() для корректных отсутпов с enableEdgeToEdge()
    - .padding(16.dp) - отступ от экрана
    - .padding(12.dp) - отступ текста внутри синего .background

--- app/src/main/java/com/example/practicumcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicumcompose/MainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +25,18 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun SayHello() {
-        Text("Hello")
+        Text(
+            text = "Hello",
+            modifier = Modifier
+                .padding(16.dp)
+                .systemBarsPadding() // 👈 ВАЖНО! Добавляет отступы для статус бара и навигации оставляем  enableEdgeToEdge()
+
+                .background(
+                    color = Color(0xFFE1F5FE),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(12.dp)
+        )
     }
 }
 
