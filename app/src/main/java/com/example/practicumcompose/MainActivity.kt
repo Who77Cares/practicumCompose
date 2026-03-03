30c6a4d4f498382fffb65cebc0a3d16c269c720f
Maxim <drensymax@gmail.com>
2026-03-03 19:32:12 +0300

    1.1 запуск

--- app/src/main/java/com/example/practicumcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicumcompose/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            SayHello()
         }
+    }
+
+    @Composable
+    private fun SayHello() {
+        Text("Hello")
     }
 }
 
